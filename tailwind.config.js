a939ffe580efc4f4384bd8f127bd9641249d7e86
Adam Stępień <vesemir2k@gmail.com>
2024-04-08 23:45:36 +0200

    Font size configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,43 @@ export default {
       serif: ['"Lora"', 'serif'],
       mono: ['"Inconsolata"', 'monospace']
     },
+    fontSize: {
+      bodyS: [
+        '14px',
+        {
+          lineHeight: '15px',
+          fontWeight: '400'
+        }
+      ],
+      bodyM: [
+        '18px',
+        {
+          lineHeight: '24px',
+          fontWeight: '400'
+        }
+      ],
+      headingS: [
+        '20px',
+        {
+          lineHeight: '24px',
+          fontWeight: '400'
+        }
+      ],
+      headingM: [
+        '24px',
+        {
+          lineHeight: '29px',
+          fontWeight: '400'
+        }
+      ],
+      headingL: [
+        '64px',
+        {
+          lineHeight: '77px',
+          fontWeight: '700'
+        }
+      ]
+    },
     extend: {}
   },
   plugins: []
