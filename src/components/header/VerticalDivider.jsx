a72e0d699d571394312082571aab3d601a98af07
Adam Stępień <vesemir2k@gmail.com>
2024-04-10 01:39:45 +0200

    Add VerticalDivider component

---
+++ src/components/header/VerticalDivider.jsx
@@ -0,0 +1,5 @@
+const VerticalDivider = () => {
+  return <div className="border-r-[1px]"></div>;
+};
+
+export default VerticalDivider;
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,13 @@
 import Logo from './Logo';
 import SelectFonts from './SelectFonts';
+import VerticalDivider from './VerticalDivider';
 
 const Header = () => {
   return (
     <header className="flex">
       <Logo />
       <SelectFonts />
+      <VerticalDivider />
     </header>
   );
 };
