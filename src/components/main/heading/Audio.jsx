e58fe32de1cda7396b00102da6259600c68804b2
Adam Stępień <vesemir2k@gmail.com>
2024-04-19 19:52:24 +0200

    Install FontAwesome and implement Audio handling

--- src/components/main/heading/Audio.jsx
+++ src/components/main/heading/Audio.jsx
@@ -1,7 +1,44 @@
+import { faPlay, faStop } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const Audio = ({ phoneticAudio }) => {
-  return <audio controls="controls" src={phoneticAudio} />;
+  const [isPlay, setIsPlay] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlay((prevState) => !prevState);
+  };
+
+  const handlePlayButton = () => {
+    const audioElement = document.querySelector('.phonetic-audio');
+    togglePlay();
+
+    if (!isPlay) {
+      audioElement.play();
+    } else {
+      audioElement.pause();
+      audioElement.currentTime = 0;
+    }
+  };
+
+  const handleAudioEnd = () => {
+    togglePlay();
+  };
+
+  return (
+    <>
+      <audio
+        className="phonetic-audio hidden"
+        controls="controls"
+        src={phoneticAudio}
+        onEnded={handleAudioEnd}
+      />
+      <button className="h-[48px] w-[48px] rounded-[50%] bg-purple" onClick={handlePlayButton}>
+        <FontAwesomeIcon icon={isPlay ? faStop : faPlay} />
+      </button>
+    </>
+  );
 };
 
 Audio.propTypes = {
