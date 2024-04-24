eb3d523cf14cf1ba3a7f24490a4412c7052825f8
Adam Stępień <vesemir2k@gmail.com>
2024-04-24 23:17:24 +0200

    ✨ (WordNoun.jsx): Add useEffect hook to update definitions based on apiResults changes and handle cases where noun is empty or not present in apiResults.

--- src/components/main/wordNoun/WordNoun.jsx
+++ src/components/main/wordNoun/WordNoun.jsx
@@ -1,27 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { SearchContext } from '../../../context/AppContext';
 import MeaningTitle from '../MeaningTitle';
 import SectionTitle from '../sectionTitle';
 
 const WordNoun = () => {
   const { apiResults } = useContext(SearchContext);
+  const [definitions, setDefinitions] = useState([]);
 
-  const noun = apiResults[0].meanings.filter((item) => item.partOfSpeech === 'noun');
+  useEffect(() => {
+    const noun = apiResults[0].meanings.filter((item) => item.partOfSpeech === 'noun');
 
-  const definitions = noun[0].definitions.map((item, index) => (
-    <li
-      key={index}
-      className="before:content mb-[13px] flex text-textColorPrimary before:mr-[20px] before:mt-[9px] before:size-[5px] before:rounded-[50%] before:bg-purple before:pl-[5px]">
-      <p className="text-bodyMobileM">{item.definition}</p>
-    </li>
-  ));
+    setDefinitions([]);
+
+    if (noun.length > 0) {
+      const nounDefinitions = noun[0].definitions.map((item, index) => (
+        <li
+          key={index}
+          className="before:content mb-[13px] flex text-textColorPrimary before:mr-[20px] before:mt-[9px] before:size-[5px] before:rounded-[50%] before:bg-purple before:pl-[5px]">
+          <p className="text-bodyMobileM">{item.definition}</p>
+        </li>
+      ));
+      setDefinitions(nounDefinitions);
+    }
+  }, [apiResults]);
 
   return (
-    <section>
-      <SectionTitle title="noun" />
-      <MeaningTitle />
-      <ul className="list-inside list-disc">{definitions}</ul>
-    </section>
+    definitions || (
+      <section>
+        <SectionTitle title="noun" />
+        <MeaningTitle />
+        <ul className="list-inside list-disc">{definitions}</ul>
+      </section>
+    )
   );
 };
 
