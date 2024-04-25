1a91391fd001d3453f61d87d3fb923b07470d277
Adam Stępień <vesemir2k@gmail.com>
2024-04-25 16:44:36 +0200

    📝 (WordDefinitionsListItem.jsx): Update WordDefinitionsListItem component to improve readability and structure of word definitions and examples.

--- src/components/main/wordDefinitionsList/WordDefinitionsListItem.jsx
+++ src/components/main/wordDefinitionsList/WordDefinitionsListItem.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 const WordDefinitionsListItem = ({ wordDefinition }) => {
   const { definition, example } = wordDefinition;
   return (
-    <li className="before:content mb-[13px] flex text-textColorPrimary before:mr-[20px] before:mt-[9px] before:size-[5px] before:rounded-[50%] before:bg-purple before:pl-[5px] last-of-type:mb-0">
-      <p className="text-bodyMobileM dark:text-white">{definition}</p>
-      {example && <p>{example}</p>}
+    <li className="before:content mb-[13px] flex text-bodyMobileM text-textColorPrimary before:mr-[20px] before:mt-[9px] before:size-[5px] before:rounded-[50%] before:bg-purple before:pl-[5px] last-of-type:mb-0">
+      <div className="flex flex-col">
+        <p className="dark:text-white">{definition}</p>
+        {example && <p className="mt-[13px] text-textColorSecondary">{example}</p>}
+      </div>
     </li>
   );
 };
