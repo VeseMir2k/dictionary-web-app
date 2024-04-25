41266a34806af724e91723c4753a93d08120374d
Adam Stępień <vesemir2k@gmail.com>
2024-04-25 16:33:54 +0200

    📝 (WordNounSynonyms.jsx): Update how synonyms are displayed to show them as a string separated by spaces for better readability.

--- src/components/main/wordNoun/WordNounSynonyms.jsx
+++ src/components/main/wordNoun/WordNounSynonyms.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
 
 const WordNounSynonyms = ({ synonyms }) => {
+  const synonymsString = synonyms.join(' ');
+
   return (
     <div className="mt-[24px] flex text-headingMobileS text-textColorSecondary">
       <div className="mr-[24px]">Synonyms</div>
-      <div className="font-bold text-purple">{synonyms}</div>
+      <div className="font-bold text-purple">{synonymsString}</div>
     </div>
   );
 };
