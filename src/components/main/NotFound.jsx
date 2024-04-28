8bbf9979824d29fc4751008df796aefcdd9f43c1
Adam Stępień <vesemir2k@gmail.com>
2024-04-28 15:03:02 +0200

    ✨ (NotFound.jsx): Improve the UI of the NotFound component by adding more descriptive text and styling to enhance user experience.

--- src/components/main/NotFound.jsx
+++ src/components/main/NotFound.jsx
@@ -3,9 +3,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NotFound = () => {
   return (
-    <>
-      <FontAwesomeIcon icon={faFaceFrown} />
-    </>
+    <div className="text-center">
+      <FontAwesomeIcon
+        className="mt-[50px] text-[64px] text-[#ffd500] text-textColorPrimary"
+        icon={faFaceFrown}
+      />
+      <h3 className="mt-[30px] text-headingS font-bold dark:text-white">No Definitions Found</h3>
+      <p className="mt-[20px] text-textColorSecondary">
+        Sorry pal, we couldn't find definitions for the word you were looking for. You can try the
+        search again at later time or head to the web instead.
+      </p>
+    </div>
   );
 };
 
