3b3c7a8e2d216bab5d597bdec9b556ad8547c3e8
Adam Stępień <vesemir2k@gmail.com>
2024-05-04 16:22:10 +0200

    ✨ (Header.jsx): Update import path for Search component to correctly point to the Search component file 🔧 (vite.config.js): Fix syntax errors and formatting issues in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/dictionary-web-app',
-  plugins: [react()],
-})
+  base: '/dictionary-web-app',
+  plugins: [react()]
+});
