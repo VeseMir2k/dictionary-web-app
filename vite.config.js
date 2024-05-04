63d9352b1b4cc55049ade9c23c0148120a7dfd9f
Adam Stępień <vesemir2k@gmail.com>
2024-05-04 16:09:25 +0200

    📝 (vite.config.js): remove trailing slash from the base URL to improve consistency and avoid potential issues with routing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/dictionary-web-app/',
+  base:'/dictionary-web-app',
   plugins: [react()],
 })
