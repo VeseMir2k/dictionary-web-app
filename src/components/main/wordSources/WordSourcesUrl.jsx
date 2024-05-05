aabe8b14017871f1f4b9385ba99622f69875b02a
Adam Stępień <vesemir2k@gmail.com>
2024-05-05 18:52:52 +0200

    📝 (WordSourcesUrl.jsx): update styling class to add margin-left to the last element for better spacing and alignment

--- src/components/main/wordSources/WordSourcesUrl.jsx
+++ src/components/main/wordSources/WordSourcesUrl.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const WordSourcesUrl = ({ url }) => {
   return (
-    <div className="mb-[6px]">
+    <div className="mb-[6px] last-of-type:ml-[5px]">
       <a
         href={url}
         className="text-textColorPrimary underline last-of-type:mb-0 hover:text-purple dark:text-white">
